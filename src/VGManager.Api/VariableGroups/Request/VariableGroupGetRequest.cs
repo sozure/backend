6be6509e23cb66917a581c5a1315149d98c037fe
Bocz Viktor <viktor.bocz@smp.hu>
2023-08-30 10:11:14 +0200

    Created base class and removed code duplications

--- src/VGManager.Api/VariableGroups/Request/VariableGroupGetRequest.cs
+++ src/VGManager.Api/VariableGroups/Request/VariableGroupGetRequest.cs
@@ -2,22 +2,7 @@
 
 namespace VGManager.Api.VariableGroups.Request;
 
-public class VariableGroupGetRequest
+public class VariableGroupGetRequest : VariableGroupRequest
 {
-    [Required]
-    public string Organization { get; set; } = null!;
-
-    [Required]
-    public string Project { get; set; } = null!;
-
-    [Required]
-    public string PAT { get; set; } = null!;
-
-    [Required]
-    public string VariableGroupFilter { get; set; } = null!;
-
-    [Required]
-    public string KeyFilter { get; set; } = null!;
-
     public string? ValueFilter { get; set; }
 }

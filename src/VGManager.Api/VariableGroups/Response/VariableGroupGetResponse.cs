c4f6f65021959f3755b3797f6ebf28b48368590f
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-09-06 18:12:57 +0200

    Refactored responses

--- src/VGManager.Api/VariableGroups/Response/VariableGroupGetResponse.cs
+++ src/VGManager.Api/VariableGroups/Response/VariableGroupGetResponse.cs
@@ -2,6 +2,7 @@
 
 public class VariableGroupGetResponse
 {
+    public string Project { get; set; } = null!;
     public string VariableGroupName { get; set; } = null!;
     public string VariableGroupKey { get; set; } = null!;
     public string VariableGroupValue { get; set; } = null!;

663cca8c27d0062fbb78e958903cf37b3ea96fa0
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-09-16 13:16:36 +0200

    Tidy up

--- src/VGManager.Api/MapperProfiles/SecretProfile.cs
+++ src/VGManager.Api/MapperProfiles/SecretProfile.cs
@@ -8,12 +8,13 @@ namespace VGManager.Api.MapperProfiles;
 public class SecretProfile : Profile
 {
     public SecretProfile()
-    {
+    { 
+        CreateMap<SecretRequest, SecretModel>();
+        CreateMap<SecretCopyRequest, SecretCopyModel>();
+
         CreateMap<SecretResultModel, SecretGetResponse>();
         CreateMap<SecretResultsModel, SecretsGetResponse>();
         CreateMap<DeletedSecretResultModel, DeletedSecretGetResponse>();
         CreateMap<DeletedSecretResultsModel, DeletedSecretsGetResponse>();
-        CreateMap<SecretRequest, SecretModel>();
-        CreateMap<SecretCopyRequest, SecretCopyModel>();
     }
 }
--- src/VGManager.Services/Models/VariableGroups/Requests/VariableGroupAddModel.cs
+++ src/VGManager.Services/Models/VariableGroups/Requests/VariableGroupAddModel.cs
@@ -1,13 +1,11 @@
-﻿
+
 using System.ComponentModel.DataAnnotations;
 
 namespace VGManager.Services.Models.VariableGroups.Requests;
 
 public class VariableGroupAddModel : VariableGroupModel
 {
-    [Required]
     public string Key { get; set; } = null!;
 
-    [Required]
     public string Value { get; set; } = null!;
 }

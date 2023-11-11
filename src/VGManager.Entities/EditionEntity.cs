6f4fadd4fa6ae59baf11549e7da72d0f97c89532
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-11-11 12:21:29 +0100

    Changed entity

--- src/VGManager.Entities/EditionEntity.cs
+++ src/VGManager.Entities/EditionEntity.cs
@@ -3,6 +3,5 @@ namespace VGManager.Entities;
 public class EditionEntity: OperationEntity
 {
     public string Key { get; set; } = null!;
-    public string OldValue { get; set; } = null!;
     public string NewValue { get; set; } = null!;
 }

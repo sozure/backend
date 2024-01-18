b6f3dc462797630876fe013b9a7dad0c24f98e2f
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-01-18 13:40:30 +0100

    Removed projects

--- src/VGManager.Entities/SecretEntities/KeyVaultCopyEntity.cs
+++
@@ -1,7 +0,0 @@
-namespace VGManager.Entities.SecretEntities;
-
-public class KeyVaultCopyEntity : SecretBaseEntity
-{
-    public string OriginalKeyVault { get; set; } = null!;
-    public string DestinationKeyVault { get; set; } = null!;
-}

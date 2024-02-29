927def404ef97782293ee7f8e2005a10231236c3
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2024-02-29 18:52:58 +0100

    Eliminated duplications

--- src/VGManager.Services/GitFileService.cs
+++ src/VGManager.Services/GitFileService.cs
@@ -1,3 +1,5 @@
+using Microsoft.TeamFoundation.WorkItemTracking.Process.WebApi.Models;
+using System.Data;
 using System.Text.Json;
 using VGManager.Adapter.Models.Kafka;
 using VGManager.Adapter.Models.Requests;
@@ -20,43 +22,17 @@ public class GitFileService(
         CancellationToken cancellationToken = default
         )
     {
-        var request = new GitFileBaseRequest<string>()
-        {
-            Organization = organization,
-            PAT = pat,
-            Branch = branch,
-            RepositoryId = repositoryId,
-            AdditionalInformation = fileName,
-        };
+        var commandType = CommandTypes.GetFilePathRequest;
 
-        (var isSuccess, var response) = await adapterCommunicator.CommunicateWithAdapterAsync(
-            request,
-            CommandTypes.GetFilePathRequest,
+        return await GetInformationAsync(
+            commandType,
+            organization,
+            pat,
+            repositoryId,
+            branch,
+            fileName,
             cancellationToken
             );
-
-        if (!isSuccess)
-        {
-            return (AdapterStatus.Unknown, Enumerable.Empty<string>());
-        }
-
-        var result = JsonSerializer.Deserialize<BaseResponse<Dictionary<string, object>>>(response)?.Data;
-
-        if (result is null)
-        {
-            return (AdapterStatus.Unknown, Enumerable.Empty<string>());
-        }
-
-        var isParseCompleted = int.TryParse(result["Status"].ToString(), out var i);
-
-        if (!isParseCompleted)
-        {
-            return (AdapterStatus.Unknown, Enumerable.Empty<string>());
-        }
-
-        var status = (AdapterStatus)i;
-        var res = JsonSerializer.Deserialize<List<string>>(result["Data"].ToString() ?? "[]") ?? [];
-        return (status, res);
     }
 
     public async Task<(AdapterStatus, IEnumerable<string>)> GetConfigFilesAsync(
@@ -68,18 +44,41 @@ public class GitFileService(
         CancellationToken cancellationToken = default
         )
     {
+        var commandType = CommandTypes.GetConfigFilesRequest;
+
+        return await GetInformationAsync(
+            commandType, 
+            organization, 
+            pat, 
+            repositoryId, 
+            branch, 
+            extension, 
+            cancellationToken
+            );
+    }
+
+    private async Task<(AdapterStatus, IEnumerable<string>)> GetInformationAsync(
+        string commandType,
+        string organization, 
+        string pat, 
+        string repositoryId, 
+        string branch,
+        string? additionalInformation,
+        CancellationToken cancellationToken
+        )
+    {
         var request = new GitFileBaseRequest<string>()
         {
             Organization = organization,
             PAT = pat,
             Branch = branch,
             RepositoryId = repositoryId,
-            AdditionalInformation = extension,
+            AdditionalInformation = additionalInformation,
         };
 
         (var isSuccess, var response) = await adapterCommunicator.CommunicateWithAdapterAsync(
             request,
-            CommandTypes.GetConfigFilesRequest,
+            commandType,
             cancellationToken
             );
 
